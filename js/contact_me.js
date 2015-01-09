f2074f3244604583be0f8cb7b1e8b0f24d50fa30
Jason Ost <jasonost@gmail.com>
2015-01-09 12:46:09 -0800

    Trying to fix email

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,11 +12,6 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
             $.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
@@ -45,7 +40,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding! Feel free to reach me via one of the channels below.");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
